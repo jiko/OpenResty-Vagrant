3472b1c71402a1dc395f8b0568a1b6815de7e314
James Koppen <jk@jameskoppen.com>
2013-10-24 00:11:04 -0500

    OpenResty-Vagrant. Debian 7. No frills.

---
+++ install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# HT github.com/benagricola/ngx-openresty-build
+# run as root
+
+# creating openresty group if he isn't already there
+if ! getent group openresty >/dev/null; then
+    addgroup --system openresty >/dev/null
+fi
+
+# creating openresty user if he isn't already there
+if ! getent passwd openresty >/dev/null; then
+    adduser \
+      --system \
+      --disabled-login \
+      --ingroup openresty \
+      --no-create-home \
+      --home /nonexistent \
+      --gecos "openresty user" \
+      --shell /bin/false \
+      openresty  >/dev/null
+fi
+
+mkdir /var/cache/openresty
+cp etc-initd-openresty.sh /etc/init.d/openresty.sh
+
+VERSION=${1:-"1.4.2.9"}
+PKG=ngx_openresty-$VERSION
+
+apt-get install -y libpcre3-dev build-essential libssl-dev sudo libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make
+
+wget http://openresty.org/download/${PKG}.tar.gz
+# for the paranoid; may need some tweaks to work right
+#wget http://openresty.org/download/${PKG}.tar.gz.asc
+#gpg --keyserver pgpkeys.mit.edu --recv-key A0E98066
+#gpg --fingerprint A0E98066
+#gpg --verify ${PKG}.tar.gz.asc ${PKG}.tar.gz
+
+tar xzvf ${PKG}.tar.gz
+cd $PKG
+./configure --with-luajit --prefix=/etc/openresty/ \
+    --sbin-path=/usr/sbin/openresty \
+    --conf-path=/etc/openresty/openresty.conf \
+    --error-log-path=/var/log/openresty/error.log \
+    --http-log-path=/var/log/openresty/access.log \
+    --pid-path=/var/run/openresty.pid \
+    --lock-path=/var/run/openresty.lock \
+    --http-client-body-temp-path=/var/cache/openresty/client_temp \
+    --http-proxy-temp-path=/var/cache/openresty/proxy_temp \
+    --http-fastcgi-temp-path=/var/cache/openresty/fastcgi_temp \
+    --http-uwsgi-temp-path=/var/cache/openresty/uwsgi_temp \
+    --http-scgi-temp-path=/var/cache/openresty/scgi_temp \
+    --user=openresty --group=openresty
+
+make
+make install
+
+if [ -x "/etc/init.d/openresty" ]; then
+    if [ -f "/var/run/openresty.pid" ] && kill -0 `cat /var/run/openresty.pid` >/dev/null; then
+        /etc/init.d/openresty upgrade || true
+    else
+        update-rc.d openresty defaults >/dev/null
+        if [ -x "`which invoke-rc.d 2>/dev/null`" ]; then
+            invoke-rc.d openresty start || true
+        else
+            /etc/init.d/openresty start || true
+        fi
+    fi
+fi
