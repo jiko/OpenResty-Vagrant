0a8452e82f52c9a8f0d629efab18a56c0087ab99
James Koppen <jk@jameskoppen.com>
2013-11-21 19:36:39 -0600

    OpenResty version bump.

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ mkdir /var/cache/openresty || true
 cp initd /etc/init.d/openresty || true
 chmod +x /etc/init.d/openresty || true
 
-VERSION=${1:-"1.4.3.4"}
+VERSION=${1:-"1.4.3.6"}
 PKG=ngx_openresty-$VERSION
 
 # see http://openresty.org/#Installation
